8986561c89fc950bcc79847dbd62928b4578d414
Ariel <arielamelia@knights.ucf.edu>
2017-04-09 19:45:24 -0400

    Text animation

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+function(){
+  document.addEventListener('DOMContentLoaded', function(){
+      Typed.new('.element', {
+        strings: ["Once upon a time there were four little Rabbits, and their names were— Flopsy, Mopsy, Cotton-tail, and Peter.", "They lived with their Mother in a sand-bank, underneath the root of a very big fir-tree."],
+        typeSpeed: 0
+      });
+  });
+}
