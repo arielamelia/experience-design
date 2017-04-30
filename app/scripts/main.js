9c8a82a5b17a518278a5a3ec08e2155e948b8f08
Ariel <arielamelia@knights.ucf.edu>
2017-04-30 15:04:48 -0400

    copyright added to clipsrt

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -18,8 +18,7 @@ $(function() {
     document.addEventListener('DOMContentLoaded', function(){
       Typed.new('#story1', {
           strings: ['Once upon a time there were four little Rabbits, and their names were— Flopsy, Mopsy, Cotton-tail, and Peter. They lived with their Mother in a sand-bank, underneath the root of a very big fir-tree.'],
-          typeSpeed: 0
-        });
+          typeSpeed: 0});
       });
 
 })
