5fc1441beea15c63a4186fa2fa37db08590fbb44
Ariel <arielamelia@knights.ucf.edu>
2017-04-30 21:01:04 -0400

    click working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,7 @@ $(function() {
 
     $('.scrollspy').scrollSpy();
     dragDrop();
-    typed();
+    document.getElementById("audio").addEventListener("click", typed);
 });
 
 function dragDrop(){
@@ -83,7 +83,7 @@ function typed(){
     typespeed: 5
   });*/
   $("#story1").typed({
-    strings: ["<h1>The Tale of Peter Rabbit</h1> ^500<h2> by Beatrix Potter</h2> ^10000","Once upon a time there were four little Rabbits, and their names were--Flopsy, Mopsy, Cotton-tail, and Peter.<br>They lived with their Mother in a sand-bank, underneath the root of a very big fir-tree.<br> 'Now my dears,' said old Mrs. Rabbit one morning, 'you may go into the fields or down the lane, but don't go into Mr. McGregor's garden: your Father had an accident there; he was put in a pie by Mrs. McGregor.'",
+    strings: ["<h1>The Tale of Peter Rabbit</h1> ^500<h2> by Beatrix Potter</h2> ^3000","Once upon a time there were four little Rabbits, and their names were--Flopsy, Mopsy, Cotton-tail, and Peter.<br>^1000They lived with their Mother in a sand-bank, underneath the root of a very big fir-tree.<br>^1000 'Now my dears,' said old Mrs. Rabbit one morning, 'you may go into the fields or down the lane, but don't go into Mr. McGregor's garden:^1000 your Father had an accident there; he was put in a pie by Mrs. McGregor.'",/*
     "'Now run along, and don't get into mischief. I am going out.'<br> Then old Mrs. Rabbit took a basket and her umbrella, and went through the wood to the baker's. She bought a loaf of brown bread and five currant buns.",
     "Flopsy, Mopsy, and Cottontail, who were good little bunnies, went down the lane to gather blackberries: But Peter, who was very naughty, ran straight away to Mr. McGregor's garden, and squeezed under the gate!",
     "First he ate some lettuces and some French beans; and then he ate some radishes; And then, feeling rather sick, he went to look for some parsley.","But round the end of a cucumber frame, whom should he meet but Mr. McGregor! <br>Mr. McGregor was on his hands and knees planting out young cabbages,but he jumped up and ran after Peter, waving a rake and calling out,<br> 'Stop thief!'",
@@ -94,9 +94,9 @@ function typed(){
     "Then he tried to find his way straight across the garden, but he became more and more puzzled. Presently, he came to a pond where Mr. McGregor filled his water-cans. A white cat was staring at some gold-fish, she sat very, very still, but now and then the tip of her tail twitched as if it were alive. Peter thought it best to go away without speaking to her; he had heard about cats from his cousin, little Benjamin Bunny.",
     "He went back towards the tool-shed, but suddenly, quite close to him, he heard the noise of a hoe--scr-r-ritch, scratch, scratch, scritch. Peter scuttered underneath the bushes. But presently, as nothing happened, he came out, and climbed upon a wheelbarrow and peeped over. The first thing he saw was Mr. McGregor hoeing onions. His back was turned towards Peter, and beyond him was the gate!",
     "Peter got down very quietly off the wheelbarrow; and started running as fast as he could go, along a straight walk behind some black-currant bushes. Mr. McGregor caught sight of him at the corner, but Peter did not care. He slipped underneath the gate, and was safe at last in the wood outside the garden.", "Mr. McGregor hung up the little jacket and the shoes for a scare-crow to frighten the blackbirds. Peter never stopped running or looked behind him till he got home to the big fir-tree.<br>He was so tired that he flopped down upon the nice soft sand on the floor of the rabbit-hole and shut his eyes. His mother was busy cooking; she wondered what he had done with his clothes. It was the second little jacket and pair of shoes that Peter had lost in a fortnight!",
-    "I am sorry to say that Peter was not very well during the evening. <br> His mother put him to bed, and made some camomile tea; and she gave a dose of it to Peter! 'One table-spoonful to be taken at bed-time.'<br> But Flopsy, Mopsy, and Cotton-tail had bread and milk and blackberries for supper.<br>THE END"],
-    typeSpeed: 5,
-    startDelay: 2,
+    "I am sorry to say that Peter was not very well during the evening. <br> His mother put him to bed, and made some camomile tea; and she gave a dose of it to Peter! 'One table-spoonful to be taken at bed-time.'<br> But Flopsy, Mopsy, and Cotton-tail had bread and milk and blackberries for supper.<br>THE END"*/],
+    typeSpeed: 20,
+    startDelay: 200,
     showCursor: false,
     contentType: 'html'
   });
